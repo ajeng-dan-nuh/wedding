a751bc5bfa1d847f5c1aba4ac2ea55da744b90e4
ajeng-dan-nuh <suratundangan2025@gmail.com>
2025-10-04 22:39:11 +0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,36 @@
+import {home} from "./js/home.js";
+import {bride} from "./js/bride.js";
+import {time} from "./js/time.js";
+import {galeri} from "./js/galeri.js";
+import {wishas} from "./js/wishas.js";
+import {navbar} from "./js/navbar.js";
+import {welcome} from "./js/welcome.js";
+import {loveStory} from "./js/lovestory.js";
+
+// load content
+document.addEventListener('DOMContentLoaded', () => {
+    AOS.init();
+
+    welcome();
+    navbar();
+    home();
+    bride()
+    time();
+    galeri();
+    wishas();
+    loveStory();
+});
+
+// Ambil parameter "to" dari URL
+const urlParams = new URLSearchParams(window.location.search);
+const guest = urlParams.get("to");
+
+// Target elemen <p id="guest-name">
+const guestNameElement = document.getElementById("guest-name");
+
+// Kalau ada parameter "to", tulis "Kepada [Nama]"
+if (guest && guestNameElement) {
+  guestNameElement.textContent = `Kepada ${guest}`;
+} else if (guestNameElement) {
+  guestNameElement.textContent = "Kepada Bapak/Ibu/Saudara/i";
+}
